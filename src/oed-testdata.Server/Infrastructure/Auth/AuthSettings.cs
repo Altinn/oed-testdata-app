21587210f92f908fa1577935cda9da622af8ffaf
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2024-12-02 15:23:17 +0100

    Basic auth

---
+++ src/oed-testdata.Server/Infrastructure/Auth/AuthSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace oed_testdata.Server.Infrastructure.Auth
+{
+    public class AuthSettings
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
--- src/oed-testdata.Server/Program.cs
+++ src/oed-testdata.Server/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 using oed_testdata.Server.Infrastructure.Altinn;
+using oed_testdata.Server.Infrastructure.Auth;
 using oed_testdata.Server.Infrastructure.TestdataStore;
 using oed_testdata.Server.Oed;
 using oed_testdata.Server.Services;
@@ -7,6 +9,31 @@ using oed_testdata.Server.Testdata.Estate;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddCors(options =>
+        options.AddPolicy("Dev", configurePolicy =>
+        {
+            // Allow multiple methods
+            configurePolicy.WithMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .WithHeaders(
+                    HeaderNames.Accept,
+                    HeaderNames.ContentType,
+                    HeaderNames.Authorization)
+                .AllowCredentials()
+                .SetIsOriginAllowed(origin =>
+                {
+                    if (string.IsNullOrWhiteSpace(origin)) return false;
+                    if (origin.ToLower().StartsWith("https://localhost")) return true;
+                    return false;
+                });
+        })
+    );
+}
+
+builder.Services.AddBasicAuthentication(builder.Configuration, builder.Environment); 
+builder.Services.AddAuthorization();
 
 builder.Services.AddOpenApi();
 
@@ -19,6 +46,20 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("Dev");
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseCookiePolicy(new CookiePolicyOptions
+{
+    Secure = CookieSecurePolicy.Always
+});
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -26,11 +67,11 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+app.MapBasicAuthenticationEndpoints();
 app.MapEstateEndpoints();
 app.MapOedInstanceEndpoints();
 
 app.UseHttpsRedirection();
-
 
 app.MapGet("/test", async ([FromServices] ITestService testService) =>
 {

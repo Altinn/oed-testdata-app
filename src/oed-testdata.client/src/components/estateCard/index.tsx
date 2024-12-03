9d4681c3420d7f352932127efb5618086dee72cd
Thomas <thomas.ostdahl@digdir.no>
2024-12-03 13:36:11 +0100

    Add post to reset estate

--- src/oed-testdata.client/src/components/estateCard/index.tsx
+++ src/oed-testdata.client/src/components/estateCard/index.tsx
@@ -1,14 +1,37 @@
-import { Button, Heading, Label } from "@digdir/designsystemet-react";
+import { Button, Heading, Label, Spinner } from "@digdir/designsystemet-react";
 import "./style.css";
 import CopyToClipboard from "../copyToClipboard";
 import { ArrowCirclepathIcon } from "@navikt/aksel-icons";
 import { Estate } from "../../interfaces/IEstate";
+import { ESTATE_API } from "../../utils/constants";
+import { useState } from "react";
 
 interface IProps {
   data: Estate;
 }
 
 export default function EstateCard({ data }: IProps) {
+  const [loading, setLoading] = useState(false);
+
+  const handleResetEstate = async () => {
+    setLoading(true);
+    const response = await fetch(ESTATE_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ estateSsn: data.estateSsn }),
+    });
+
+    /* TODO: handle response success or failure with toasts */
+
+    if (!response.ok) {
+      console.error("Error resetting estate:", response.statusText);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <article className="card">
       <Heading level={2} size="md" spacing className="card__heading">
@@ -34,9 +57,23 @@ export default function EstateCard({ data }: IProps) {
       </section>
 
       <div className="card__footer">
-        <Button color="first" size="sm">
-          <ArrowCirclepathIcon title="a11y-title" fontSize="1.5rem" />
-          Nullstill
+        <Button
+          color="first"
+          size="sm"
+          onClick={handleResetEstate}
+          aria-disabled={loading}
+        >
+          {loading ? (
+            <>
+              <Spinner variant="interaction" title="loading" size="sm" />
+              Laster...
+            </>
+          ) : (
+            <>
+              <ArrowCirclepathIcon title="a11y-title" fontSize="1.5rem" />
+              Nullstill
+            </>
+          )}
         </Button>
       </div>
     </article>

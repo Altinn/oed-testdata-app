75ec366a3216a0a66a665ca06b4aaffe618791c6
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2025-02-27 13:32:14 +0100

    Add new hidden estate

--- src/oed-testdata.Server/Testdata/Estate/EstateConstants.cs
+++ src/oed-testdata.Server/Testdata/Estate/EstateConstants.cs
@@ -9,6 +9,7 @@
             "21885399468",          // KM - STRAFFET_PINSETT
             "05819496309",          // LOK - VEIK_TELEFONKATALOG    (Både den døde og alle relasjoner har adressebeskyttelse)
             "22928597168",          // Uskifte - TRIST FJELL
+            "27826499058",          // LOK - AITORISERT ALV         (Den døde har hatt mange historiske bosteder)
             //"21899097235",          // LOK - ELASTISK_KOKEPLATE     (Inneholder en arving (barn) som er under 18 år)
         ];
     }

78ce95ca55fdf4afcb0c6daded80a2d9c6cd5e98
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2025-02-28 11:01:34 +0100

    Synliggjør bo

--- src/oed-testdata.Server/Testdata/Estate/EstateConstants.cs
+++ src/oed-testdata.Server/Testdata/Estate/EstateConstants.cs
@@ -7,7 +7,7 @@
             "06888298329",          // TOE - DISKRET_LEVERANDØR
             "16873948381",          // DRJ - UPERSONLIG_SYRE
             "21885399468",          // KM - STRAFFET_PINSETT
-            "05819496309",          // LOK - VEIK_TELEFONKATALOG    (Både den døde og alle relasjoner har adressebeskyttelse)
+            //"05819496309",          // LOK - VEIK_TELEFONKATALOG    (Både den døde og alle relasjoner har adressebeskyttelse)
             "22928597168",          // Uskifte - TRIST FJELL
             "27826499058",          // LOK - AITORISERT ALV         (Den døde har hatt mange historiske bosteder)
             //"21899097235",          // LOK - ELASTISK_KOKEPLATE     (Inneholder en arving (barn) som er under 18 år)
